aed8529f869939cedd6cda6f3470bb86c7022601
Mart0 <martin.kiri98@gmail.com>
2022-09-21 22:03:24 +0200

    Centrado del contenido de los productos y el carrito

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Función para poder hacer responsive la barra de navegación
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
