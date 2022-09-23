cbd4af1cbd06c456ab2b319c0407bd094c7c35f6
Mart0 <martin.kiri98@gmail.com>
2022-09-23 15:49:52 +0200

    Implementación del valor de los objetos y la posiblidad de arrastrar los tikets

--- js/main.js
+++ js/main.js
@@ -1,10 +1,50 @@
-// Función para poder hacer responsive la barra de navegación
+//  inicio del pro
+let precioTotal = 0;
+let totalProductos = document.getElementById("precio");
+totalProductos.innerHTML = `${ precioTotal} €`;
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+//  DECLARACIÓn de los objetos
+let objetos = [
+    {
+        id: "coches",
+        precio: 3
+    },
+    {
+        id: "tren",
+        precio:5
+    },
+    {
+        id: "noria",
+        precio:8
+    },
+    {
+        id: "tiketgratis",
+        precio: 0.5
+    },
+    {
+        id: "avion",
+        precio: 10
+    },
+    {
+        id: "comida",
+        precio:5
+    },
+    {
+        id: "miedo",
+        precio:10
+    },
+    {
+        id: "platillo",
+        precio:10
+    },
+    {
+        id: "blobo",
+        precio:10
+    },
+    {
+        id: "moto",
+        precio:8
     }
-  }
+
+];
+
