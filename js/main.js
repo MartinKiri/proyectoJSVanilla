20ff01ed8091513c0ddef17faa3ab7ad88be045d
Mart0 <martin.kiri98@gmail.com>
2022-09-25 23:55:01 +0200

    Cuenta cuanto te gastarás en la compra de los tikets

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //  inicio del pro
 let precioTotal = 0;
-let totalProductos = document.getElementById("precio");
-totalProductos.innerHTML = `${ precioTotal} €`;
+let totalProductos = document.getElementById("totalProductos");
+totalProductos.innerHTML = `${precioTotal} €`;
 
 //  DECLARACIÓn de los objetos
 let objetos = [
@@ -11,11 +11,11 @@ let objetos = [
     },
     {
         id: "tren",
-        precio:5
+        precio: 5
     },
     {
         id: "noria",
-        precio:8
+        precio: 8
     },
     {
         id: "tiketgratis",
@@ -27,24 +27,42 @@ let objetos = [
     },
     {
         id: "comida",
-        precio:5
+        precio: 5
     },
     {
         id: "miedo",
-        precio:10
+        precio: 10
     },
     {
         id: "platillo",
-        precio:10
+        precio: 35
     },
     {
-        id: "blobo",
-        precio:10
+        id: "globo",
+        precio: 10
     },
     {
         id: "moto",
-        precio:8
+        precio: 8
     }
-
 ];
 
+/////////////////////////////////////////////////////
+
+const drag = (ev) => {
+    ev.dataTransfer.setData("text", ev.target.id);
+
+};
+const allowDrop = (ev) => {
+    ev.preventDefault();
+};
+const drop = (ev) => {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+
+    let tiket = objetos.find(objeto => {
+        return objeto.id == data;
+    });
+    precioTotal += tiket.precio;
+    totalProductos.innerHTML = `${precioTotal} €`;
+};
