4b62b9372e342a9d010dca2760c346cc47a0be41
Mart0 <martin.kiri98@gmail.com>
2022-09-29 00:20:22 +0200

    implementación de la parte de tiempo de cada atracción

--- js/main.js
+++ js/main.js
@@ -1,49 +1,61 @@
-//  inicio del pro
+//  inicio del programa
 let precioTotal = 0;
+let tiempoTotal = 0;
 let totalProductos = document.getElementById("totalProductos");
 totalProductos.innerHTML = `${precioTotal} €`;
+
 
 //  DECLARACIÓn de los objetos
 let objetos = [
     {
         id: "coches",
-        precio: 3.50
+        precio: 3.50,
+        tiempo: 180 / 60
     },
     {
         id: "tren",
-        precio: 6.80
+        precio: 6.80,
+        tiempo: 140 / 60
     },
     {
         id: "noria",
-        precio: 8
+        precio: 8,
+        tiempo: 300 / 60
     },
     {
         id: "tiketgratis",
-        precio: 1
+        precio: 1,
+        tiempo: 0 / 60
     },
     {
         id: "avion",
-        precio: 10
+        precio: 10,
+        tiempo: 220 / 60
     },
     {
         id: "comida",
-        precio: 2.5
+        precio: 2.5,
+        tiempo: 0 / 60
     },
     {
         id: "miedo",
-        precio: 8
+        precio: 8,
+        tiempo: 300 / 60
     },
     {
         id: "platillo",
-        precio: 4
+        precio: 4,
+        tiempo: 250 / 60
     },
     {
         id: "globo",
-        precio: 60
+        precio: 60,
+        tiempo: 480 / 60
     },
     {
         id: "moto",
-        precio: 5
+        precio: 5,
+        tiempo: 200 / 60
     }
 ];
 
@@ -59,7 +71,9 @@ const allowDrop = (ev) => {
 
 const vaciar = () => {
     precioTotal = 0;
+    tiempoTotal = 0;
     totalProductos.innerHTML = precioTotal + "€";
+    totalTiempo.innerHTML = tiempoTotal + " minutos";
 };
 
 const drop = (ev) => {
@@ -71,8 +85,13 @@ const drop = (ev) => {
     });
     precioTotal += tiket.precio;
     totalProductos.innerHTML = `${precioTotal} €`;
-    if(precioTotal >= 100) {
+    totalTiempo.innerHTML = `${tiempoTotal}`;
+
+    //funcion en la que cuenta cuanto tiempo vas a estar en las attrasccions
+    tiempoTotal += tiket.tiempo;
+    totalTiempo.innerHTML = `${tiempoTotal} minutos`;
+
+    if (precioTotal >= 100) {
         alert("Te vas a gastar más de 100€,");
     }
 };
-//funcion contar tiempo de las atracciones//
